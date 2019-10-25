cb8b252adc9de904b7a9bb1a7e85ddd39e987d68
Ryota Seki <34875734+ryotabs@users.noreply.github.com>
2019-10-25 13:21:20 -1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+For details, please see http://ics-software-engineering.github.io/meteor-application-template-react/
---
+++ app/imports/ui/pages/AddStuff.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Stuffs } from '/imports/api/stuff/Stuff';
+import { Grid, Segment, Header } from 'semantic-ui-react';
+import AutoForm from 'uniforms-semantic/AutoForm';
+import TextField from 'uniforms-semantic/TextField';
+import NumField from 'uniforms-semantic/NumField';
+import SelectField from 'uniforms-semantic/SelectField';
+import SubmitField from 'uniforms-semantic/SubmitField';
+import ErrorsField from 'uniforms-semantic/ErrorsField';
+import swal from 'sweetalert';
+import { Meteor } from 'meteor/meteor';
+import 'uniforms-bridge-simple-schema-2'; // required for Uniforms
+import SimpleSchema from 'simpl-schema';
+
+/** Create a schema to specify the structure of the data to appear in the form. */
+const formSchema = new SimpleSchema({
+  name: String,
+  quantity: Number,
+  condition: {
+    type: String,
+    allowedValues: ['excellent', 'good', 'fair', 'poor'],
+    defaultValue: 'good',
+  },
+});
+
+/** Renders the Page for adding a document. */
+class AddStuff extends React.Component {
+
+  /** On submit, insert the data. */
+  submit(data, formRef) {
+    const { name, quantity, condition } = data;
+    const owner = Meteor.user().username;
+    Stuffs.insert({ name, quantity, condition, owner },
+      (error) => {
+        if (error) {
+          swal('Error', error.message, 'error');
+        } else {
+          swal('Success', 'Item added successfully', 'success');
+          formRef.reset();
+        }
+      });
+  }
+
+  /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
+  render() {
+    let fRef = null;
+    return (
+        <Grid container centered>
+          <Grid.Column>
+            <Header as="h2" textAlign="center">Add Stuff</Header>
+            <AutoForm ref={ref => { fRef = ref; }} schema={formSchema} onSubmit={data => this.submit(data, fRef)} >
+              <Segment>
+                <TextField name='name'/>
+                <NumField name='quantity' decimal={false}/>
+                <SelectField name='condition'/>
+                <SubmitField value='Submit'/>
+                <ErrorsField/>
+              </Segment>
+            </AutoForm>
+          </Grid.Column>
+        </Grid>
+    );
+  }
+}
+
+export default AddStuff;
