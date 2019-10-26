c43096322001254b6a08fe4594b3324d84280be9
Brody <brodyuehara@gmail.com>
2019-10-26 11:37:58 -1000

    New login

--- app/imports/ui/components/MiddleHP.jsx
+++ app/imports/ui/components/MiddleHP.jsx
@@ -1,24 +1,37 @@
 import React from 'react';
-import { Menu, Grid, Button } from 'semantic-ui-react';
+import { Button, Divider, Form, Grid, Segment } from 'semantic-ui-react';
 
 export default class MiddleHP extends React.Component {
   render() {
-    const gridStyle = { height: '500px' };
     return (
-        <Menu borderless className="middle">
-          <Grid container verticalAlign={'middle'} style={gridStyle}>
-            <Grid.Row columns='two'>
-              <Grid.Column>
-                New User:
-                <Button>Sign Up</Button>
-              </Grid.Column>
-              <Grid.Column>
-                Returning User:
-                <Button>Log In</Button>
-              </Grid.Column>
-            </Grid.Row>
+        <Segment placeholder>
+          <Grid columns={2} relaxed='very' stackable>
+            <Grid.Column>
+              <Form>
+                <Form.Input
+                    icon='user'
+                    iconPosition='left'
+                    label='Username'
+                    placeholder='Username'
+                />
+                <Form.Input
+                    icon='lock'
+                    iconPosition='left'
+                    label='Password'
+                    type='password'
+                />
+
+                <Button content='Login' primary />
+              </Form>
+            </Grid.Column>
+
+            <Grid.Column verticalAlign='middle'>
+              <Button content='Sign up' icon='signup' size='big' />
+            </Grid.Column>
           </Grid>
-        </Menu>
+
+          <Divider vertical>Or</Divider>
+        </Segment>
     );
   }
 }
