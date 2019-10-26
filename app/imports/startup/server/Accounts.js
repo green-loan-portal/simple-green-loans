151d0eb173f038d1e9ff3db9c001491fa6b844e3
Wiley Bui <buixx206@umn.edu>
2019-10-26 09:21:17 -1000

    Added

--- app/imports/startup/server/Accounts.js
+++ app/imports/startup/server/Accounts.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 import { Roles } from 'meteor/alanning:roles';
+import { Email } from 'meteor/email';
 
 /* eslint-disable no-console */
 
@@ -25,3 +26,33 @@ if (Meteor.users.find().count() === 0) {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
 }
+
+console.log("Passed1");
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    console.log("Passed2");
+    // process.env.MAIL_URL = "smtp://postmaster%40sandbox5ecff102054b492ab4b6959e3394e2ab.mailgun.org:7f50dcf8a84ee0a19a43f40d50a082a7-2dfb0afe-5b4fe61e@smtp.mailgun.org:587"
+    // process.env.MAIL_URL = `smtps://17buih%40gmail.com:${Meteor.settings.wileysGmailPassword}@smtp.gmail.com:465`;
+    process.env.MAIL_URL = `smtps://<email_username>%40gmail.com:<email_password>@smtp.gmail.com:465`;
+
+    let myHTMLString = "<center><img src='https://gems.hawaii.gov/wp-content/uploads/2015/01/GEMS-Logo.png' width='50%' /></center><br>";
+    myHTMLString += "Dear Wiley,<br><br>";
+    myHTMLString += "Thank you for signing up at <a href=\"https://gems.hawaii.gov\">GEMS.hawaii.gov</a>. In order for you to register, please <a href=\"https://www.test.com\">click here</a> to verify your account.<br>";
+    myHTMLString += "We are looking forward hearing you soon.<br><br>";
+    myHTMLString += "Cheers,<br>GEMS Hawaii Team";
+
+    console.log("Passed3");
+    try {
+      Email.send({
+        to: "wileyb@hawaii.edu",
+        from: "Hawaii GEMS",
+        subject: "GEMS - Email Confirmation",
+        html: myHTMLString
+      });
+      console.log("Sent");
+    } catch (e) {
+      console.log("Email error: " + e);
+    }
+  });
+}
+
