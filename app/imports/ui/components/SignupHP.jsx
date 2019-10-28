40e3200f6df9a605f74d065a1c83eeed1435209a
Brody <brodyuehara@gmail.com>
2019-10-28 09:40:43 -1000

    Added more stuff

--- app/imports/ui/components/SignupHP.jsx
+++ app/imports/ui/components/SignupHP.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Divider, Form, Grid, Segment } from 'semantic-ui-react';
 
-export default class MiddleHP extends React.Component {
+export default class SignupHP extends React.Component {
   render() {
     return (
         <Segment placeholder>
