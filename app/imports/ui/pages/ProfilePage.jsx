1e8f8567a17c97b82d24d8930dd141a5e68038c2
Trey Sumida <tasumida@hawaii.edu>
2019-11-10 14:04:04 -1000

    added application status on profile page

--- app/imports/ui/pages/ProfilePage.jsx
+++ app/imports/ui/pages/ProfilePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header, Container, Grid, Segment, Divider, Button } from 'semantic-ui-react';
+import { Header, Container, Grid, Segment, Divider, Button, Step, Icon } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 
 export default class ProfilePage extends React.Component {
@@ -18,8 +18,37 @@ export default class ProfilePage extends React.Component {
                 </Grid.Column>
 
                 <Grid.Column>
-                  <Header as="h1">View Your Application Status</Header>
-                  <Button>Click Here</Button>
+                  <Header as="h1">Your Application Status</Header>
+                  <Step.Group vertical>
+                    <Step>
+                      <Icon name='wpforms' />
+                      <Step.Content>
+                        <Step.Title>Finish Application</Step.Title>
+                        <Step.Description>Complete your application and submit</Step.Description>
+                      </Step.Content>
+                    </Step>
+                    <Step disabled>
+                      <Icon name='info' />
+                      <Step.Content>
+                        <Step.Title>HECO</Step.Title>
+                        <Step.Description>Gathering information from HECO</Step.Description>
+                      </Step.Content>
+                    </Step>
+                    <Step disabled>
+                      <Icon name='folder open' />
+                      <Step.Content>
+                        <Step.Title>Reviewing information</Step.Title>
+                        <Step.Description>We are currently reviewing your application</Step.Description>
+                      </Step.Content>
+                    </Step>
+                    <Step disabled>
+                      <Icon name='checkmark' />
+                      <Step.Content>
+                        <Step.Title>Approved/Denied</Step.Title>
+                        <Step.Description>Your application has been approved!</Step.Description>
+                      </Step.Content>
+                    </Step>
+                  </Step.Group>
                 </Grid.Column>
               </Grid.Row>
             </Grid>
