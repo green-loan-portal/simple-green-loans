41dc0535962c67b6ba9cf63160229b071185b31b
Brody <brodyuehara@gmail.com>
2019-11-19 11:53:38 -1000

    Fixed Footer Destinations

---
+++ app/imports/ui/components/PlainFooter.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+
+/** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
+class Footer extends React.Component {
+  render() {
+    return (
+        <div className="ui segment">
+          <h5 className="ui right aligned header">Built by: Team Free Labor</h5>
+        </div>
+    );
+  }
+}
+
+export default Footer;
