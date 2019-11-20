390100698bd15cb3b4712ba50caeab931f93ab2e
Brody <brodyuehara@gmail.com>
2019-11-20 13:44:13 -1000

    Update PlainFooter.jsx

--- app/imports/ui/components/PlainFooter.jsx
+++ app/imports/ui/components/PlainFooter.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
