4a1ddf9c402cdcc3a61e448678aee64355c403ff
Brody <brodyuehara@gmail.com>
2019-12-06 11:39:16 -1000

    Need to fix

--- app/imports/ui/components/Middle1HP.jsx
+++ app/imports/ui/components/Middle1HP.jsx
@@ -7,41 +7,38 @@ export default class Middle1HP extends React.Component {
     return (
         <div className={'introduction'}>
           <Container>
-          <Grid style={gridStyle}>
-            <Grid.Row columns="two">
-              <Grid.Column>
-                <Header as='h1'>
-                  What is GEMS?
-                </Header>
-                <hr/>
-                The Green Energy Market Securitization Program (GEMS) is an innovative, sustainable green financing
-                initiative designed by the Hawaii State Energy Office to make clean energy improvements more
-                affordable and accessible for Hawaii consumers. The program provides low-cost capital to finance solar
-                photovoltaic systems and other clean energy improvements for those who may otherwise have difficulty
-                obtaining financing for these projects.
-              </Grid.Column>
-              <Grid.Column>
-                <Header as='h1'>
-                  <Icon name='tasks' />
-                  Eligibility
-                </Header>
-                <hr/>
-                <List bulleted>
-                  <List.Item>Must be a Hawaiian Electric, Maui Electric, or Hawaiian Electric Light Company customer on
-                    Rate Scheduled R Project Elligibility</List.Item>
-                  <List.Item>No disconnect notice issued by the Utility within the past 12 months</List.Item>
-                  <List.Item>Minimum of 6 months of history with the Utility</List.Item>
-                  <List.Item>Households must be Low and Moderate-Income (LMI) (Defined as 140% Area Median Income per
-                    the U.S. Department of Housing & Urban Development and Hawaii Housing Finance & Development
-                    Corporation’s guidelines)
-                  </List.Item>
-                  <List.Item href='http://gems.hawaii.gov/'>
-                    Additional information can be found here
-                  </List.Item>
-                </List>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
+            <Grid style={gridStyle}>
+              <Grid.Row columns="two">
+                <Grid.Column>
+                  <Header as='h1' style={{ 'font-size': 50 }}>
+                    What is GEMS?
+                  </Header>
+                  <hr/>
+                  The GEM$ provides flexible, alternative financing options to make green energy solutions possible.
+                </Grid.Column>
+                <Grid.Column>
+                  <Header as='h1'>
+                    <Icon name='tasks'/>
+                    Eligibility
+                  </Header>
+                  <hr/>
+                  <List bulleted>
+                    <List.Item>Must be a Hawaiian Electric, Maui Electric, or Hawaiian Electric Light Company customer
+                      on
+                      Rate Scheduled R Project Eligibility</List.Item>
+                    <List.Item>No disconnect notice issued by the Utility within the past 12 months</List.Item>
+                    <List.Item>Minimum of 6 months of history with the Utility</List.Item>
+                    <List.Item>Households must be Low and Moderate-Income (LMI) (Defined as 140% Area Median Income per
+                      the U.S. Department of Housing & Urban Development and Hawaii Housing Finance & Development
+                      Corporation’s guidelines)
+                    </List.Item>
+                    <List.Item href='http://gems.hawaii.gov/'>
+                      Additional information can be found here
+                    </List.Item>
+                  </List>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
           </Container>
         </div>
     );
